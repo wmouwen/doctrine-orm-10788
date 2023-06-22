8b56a890aa9a5ecc84d4a1cbfa846fd2a7fceeba
Willem Mouwen <willem@mouwen.dev>
2023-06-22 19:06:43 +0200

    Add comments, flush manager

--- trigger_error.php
+++ trigger_error.php
@@ -4,11 +4,20 @@ require_once __DIR__ . '/bootstrap.php';
 
 global $entityManager;
 
+// Create entity with BackedEnum as primary key.
 $locale = new \Entity\Locale();
 $locale->code = \Enum\LocaleCode::Dutch;
 
+// Persist entity, clear manager.
+$entityManager->persist($locale);
+$entityManager->flush();
+$entityManager->clear();
+
+// Create entity with an association to the entity with BackedEnum key.
+// Note that the use of a proxy class is required for the error to show.
 $categoryLocale = new \Entity\CategoryLocale();
 $categoryLocale->locale = $entityManager->getReference(\Entity\Locale::class, \Enum\LocaleCode::Dutch);
 
+// Attempt to persist, throws an error.
 $entityManager->persist($categoryLocale);
 $entityManager->flush();
